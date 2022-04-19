c7776438485f79c12997f418d37b93cc9bc2ac8c
Abhishekkp42 <96106223+Abhishekkp42@users.noreply.github.com>
2022-04-19 16:14:15 +0530

    final changes

--- src/components/DateRange.jsx
+++ src/components/DateRange.jsx
@@ -1,20 +1,25 @@
 import {React,useState} from 'react'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "./styles.css";
 export const DateRange = () => {
  
         const [startDate, setStartDate] = useState(new Date("2022/02/08"));
         const [endDate, setEndDate] = useState(new Date("2022/02/10"));
         return (
           <>
+		  
+		  <div className='datediv'><h1>Start Date</h1>
             <DatePicker
               selected={startDate}
               onChange={(date) => setStartDate(date)}
               selectsStart
               startDate={startDate}
               endDate={endDate}
+			  id="date"
              // showDisabledMonthNavigation
             />
+			<h1>End Date</h1>
             <DatePicker
               selected={endDate}
               onChange={(date) => setEndDate(date)}
@@ -22,8 +27,9 @@ export const DateRange = () => {
               startDate={startDate}
               endDate={endDate}
               minDate={startDate}
+			  id="date"
             />
-            
+            </div>
           </>
         
   )
